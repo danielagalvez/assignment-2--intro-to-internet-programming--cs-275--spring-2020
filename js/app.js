cf527c0c0b3bf17f259144576bcd2681a985421e
danielagalvez <60191717+danielagalvez@users.noreply.github.com>
2020-05-04 01:01:36 -0400

    Check which box is checked

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 let button = document.querySelector('button');
 let p = document.querySelector('p');
 button.addEventListener(`click`, () => {
+    let selected = document.querySelector('input[type= "radio"]:checked');
