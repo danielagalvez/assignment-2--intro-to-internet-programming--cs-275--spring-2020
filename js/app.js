fb74490a34ff361b79e6fa7726a6cd20b311606a
danielagalvez <60191717+danielagalvez@users.noreply.github.com>
2020-05-04 01:01:46 -0400

    Display selection

--- js/app.js
+++ js/app.js
@@ -2,3 +2,4 @@ let button = document.querySelector('button');
 let p = document.querySelector('p');
 button.addEventListener(`click`, () => {
     let selected = document.querySelector('input[type= "radio"]:checked');
+    p.innerHTML = selected.value;
