0a1181091c7af7ffa418e79ca327b3f153330ac9
danielagalvez <60191717+danielagalvez@users.noreply.github.com>
2020-05-04 01:01:55 -0400

    Create prompt

--- js/app.js
+++ js/app.js
@@ -3,3 +3,4 @@ let p = document.querySelector('p');
 button.addEventListener(`click`, () => {
     let selected = document.querySelector('input[type= "radio"]:checked');
     p.innerHTML = selected.value;
+    let userInput= prompt('Enter the amount of rice you would like to make(in fluid ounces)');
